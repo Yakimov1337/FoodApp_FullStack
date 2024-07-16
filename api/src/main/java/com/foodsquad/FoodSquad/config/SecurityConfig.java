83337a3d0668d35263dfacfbda1a5fa25c42ba17
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-07-16 21:48:00 +0300

    impl: Security config

---
+++ api/src/main/java/com/foodsquad/FoodSquad/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.foodsquad.FoodSquad.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // Disable CSRF for simplicity
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll() // Allow access to Swagger UI without authentication
+                                .anyRequest().authenticated() // Secure other endpoints
+                )
+                .formLogin(formLogin -> formLogin.permitAll()) // Allow form login
+                .logout(logout -> logout.permitAll()); // Allow logout
+        return http.build();
+    }
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(csrf -> csrf.disable()) // Disable CSRF for simplicity
+//                .authorizeHttpRequests(authorizeRequests ->
+//                        authorizeRequests
+//                                .anyRequest().permitAll() // Allow access to all routes
+//                )
+//                .formLogin(formLogin -> formLogin.permitAll()) // Allow form login
+//                .logout(logout -> logout.permitAll()); // Allow logout
+//        return http.build();
+//    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
