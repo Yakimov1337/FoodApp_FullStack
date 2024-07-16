89d10f0568687e6f5a6ca55bee4787466750f440
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-07-16 21:48:16 +0300

    Create UserController.java

---
+++ api/src/main/java/com/foodsquad/FoodSquad/controller/UserController.java
@@ -0,0 +1,49 @@
+package com.foodsquad.FoodSquad.controller;
+
+import com.foodsquad.FoodSquad.model.dto.UserLoginDTO;
+import com.foodsquad.FoodSquad.model.dto.UserRegistrationDTO;
+import com.foodsquad.FoodSquad.model.dto.UserResponseDTO;
+import com.foodsquad.FoodSquad.service.AuthService;
+import com.foodsquad.FoodSquad.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@Validated
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private AuthService authService;
+
+    @GetMapping
+    public ResponseEntity<List<UserResponseDTO>> getAllUsers() {
+        List<UserResponseDTO> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> getUserById(@PathVariable Long id) {
+        UserResponseDTO user = userService.getUserById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> updateUser(@PathVariable Long id, @Valid @RequestBody UserResponseDTO userResponseDTO) {
+        UserResponseDTO updatedUser = userService.updateUser(id, userResponseDTO);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+}
