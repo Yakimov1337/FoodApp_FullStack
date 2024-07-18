1c755d4a9b1de1601cefbb438791aa1fc87fc47c
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-07-18 19:09:22 +0300

    fix:return appropriate status

--- api/src/main/java/com/foodsquad/FoodSquad/service/AuthService.java
+++ api/src/main/java/com/foodsquad/FoodSquad/service/AuthService.java
@@ -8,6 +8,8 @@ import com.foodsquad.FoodSquad.model.entity.UserRole;
 import com.foodsquad.FoodSquad.repository.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -24,26 +26,29 @@ public class AuthService {
     @Autowired
     private ModelMapper modelMapper;
 
-    public UserResponseDTO registerUser(UserRegistrationDTO userRegistrationDTO) {
+    public ResponseEntity<String> registerUser(UserRegistrationDTO userRegistrationDTO) {
+        Optional<User> existingUser = userRepository.findByEmail(userRegistrationDTO.getEmail());
+        if (existingUser.isPresent()) {
+            return new ResponseEntity<>("Email already exists", HttpStatus.CONFLICT);
+        }
         User user = new User();
         user.setEmail(userRegistrationDTO.getEmail());
         user.setPassword(passwordEncoder.encode(userRegistrationDTO.getPassword()));
         user.setRole(UserRole.NORMAL);
-        user.setImageUrl("defaultImageUrl");
+        user.setImageUrl("https://cloud.appwrite.io/v1/storage/buckets/66099552d89fbdfa20d4/files/663f1c48a822caaf325c/view?project=65ef1b8962547e24afec&mode=admin");
 
-        User savedUser = userRepository.save(user);
-        return modelMapper.map(savedUser, UserResponseDTO.class);
+        userRepository.save(user);
+        return new ResponseEntity<>("User successfully registered", HttpStatus.CREATED);
     }
 
-    public UserResponseDTO loginUser(UserLoginDTO userLoginDTO) {
+    public ResponseEntity<UserResponseDTO> loginUser(UserLoginDTO userLoginDTO) {
         Optional<User> optionalUser = userRepository.findByEmail(userLoginDTO.getEmail());
         if (optionalUser.isPresent()) {
             User user = optionalUser.get();
             if (passwordEncoder.matches(userLoginDTO.getPassword(), user.getPassword())) {
-                return modelMapper.map(user, UserResponseDTO.class);
+                return ResponseEntity.ok(modelMapper.map(user, UserResponseDTO.class));
             }
         }
-        throw new IllegalArgumentException("Invalid email or password");
+        return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
     }
-
 }
