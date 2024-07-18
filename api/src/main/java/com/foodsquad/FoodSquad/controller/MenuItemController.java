e1182c6731d7dc04481f77ea00cd181e81947302
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-07-18 19:05:33 +0300

    Create MenuItemController.java

---
+++ api/src/main/java/com/foodsquad/FoodSquad/controller/MenuItemController.java
@@ -0,0 +1,47 @@
+package com.foodsquad.FoodSquad.controller;
+
+import com.foodsquad.FoodSquad.model.dto.MenuItemDTO;
+import com.foodsquad.FoodSquad.service.MenuItemService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Validated
+@RestController
+@RequestMapping("/api/menu-items")
+@Tag(name = "4. Menu Item Management", description = "Menu Item Management API")
+public class MenuItemController {
+
+    @Autowired
+    private MenuItemService menuItemService;
+
+    @PostMapping
+    public ResponseEntity<String> createMenuItem(@Valid @RequestBody MenuItemDTO menuItemDTO) {
+        return menuItemService.createMenuItem(menuItemDTO);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MenuItemDTO> getMenuItemById(@PathVariable Long id) {
+        return menuItemService.getMenuItemById(id);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MenuItemDTO>> getAllMenuItems() {
+        return menuItemService.getAllMenuItems();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateMenuItem(@PathVariable Long id, @Valid @RequestBody MenuItemDTO menuItemDTO) {
+        return menuItemService.updateMenuItem(id, menuItemDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteMenuItem(@PathVariable Long id) {
+        return menuItemService.deleteMenuItem(id);
+    }
+}
