41af0520306f6eec96ceabee1be136fe8935f04b
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-07-18 19:06:38 +0300

    fix: annotations

--- api/src/main/java/com/foodsquad/FoodSquad/model/dto/MenuItemDTO.java
+++ api/src/main/java/com/foodsquad/FoodSquad/model/dto/MenuItemDTO.java
@@ -1,47 +1,57 @@
 package com.foodsquad.FoodSquad.model.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 import org.hibernate.validator.constraints.URL;
 
 public class MenuItemDTO {
-    @NotBlank
+
+    @NotBlank(message = "Title cannot be blank")
     private String title;
 
-    @NotBlank
+    @NotBlank(message = "Description cannot be blank")
     private String description;
 
     @URL
+    @NotBlank(message = "ImageUrl cannot be blank")
+    @Schema(defaultValue = "https://www.tastingtable.com/img/gallery/what-makes-restaurant-burgers-taste-different-from-homemade-burgers-upgrade/l-intro-1662064407.jpg")
     private String imageUrl;
 
     private Boolean defaultItem;
 
-    @NotNull
+    @NotNull(message = "Price cannot be null")
+    @Positive(message = "Total cost must be positive")
     private Double price;
 
+    @NotNull(message = "Category cannot be null")
+    @Pattern(regexp = "BURGER|PIZZA|SUSHI|PASTA|SALAD|TACOS|DESSERT|OTHER", message = "Category must be one of the following: BURGER, PIZZA, SUSHI,PASTA,SALAD,TACOS,DESSERT,OTHER")
+    @Schema(defaultValue = "BURGER")
     private String category;
 
-    public @NotBlank String getTitle() {
+    public String getTitle() {
         return title;
     }
 
-    public void setTitle(@NotBlank String title) {
+    public void setTitle(String title) {
         this.title = title;
     }
 
-    public @NotBlank String getDescription() {
+    public String getDescription() {
         return description;
     }
 
-    public void setDescription(@NotBlank String description) {
+    public void setDescription(String description) {
         this.description = description;
     }
 
-    public @URL String getImageUrl() {
+    public String getImageUrl() {
         return imageUrl;
     }
 
-    public void setImageUrl(@URL String imageUrl) {
+    public void setImageUrl(String imageUrl) {
         this.imageUrl = imageUrl;
     }
 
@@ -53,11 +63,11 @@ public class MenuItemDTO {
         this.defaultItem = defaultItem;
     }
 
-    public @NotNull Double getPrice() {
+    public Double getPrice() {
         return price;
     }
 
-    public void setPrice(@NotNull Double price) {
+    public void setPrice(Double price) {
         this.price = price;
     }
 
