c0967c070bf90c42f281163038c26b397bd0f98c
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-07-18 19:04:13 +0300

    feat: added tag names

--- api/src/main/java/com/foodsquad/FoodSquad/config/OpenApiConfig.java
+++ api/src/main/java/com/foodsquad/FoodSquad/config/OpenApiConfig.java
@@ -2,9 +2,9 @@ package com.foodsquad.FoodSquad.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.tags.Tag;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 
 @Configuration
 public class OpenApiConfig {
@@ -15,6 +15,10 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("Food Squad API")
                         .version("1.0")
-                        .description("API documentation for Food Squad application"));
+                        .description("API documentation for Food Squad application"))
+                .addTagsItem(new Tag().name("1. Authentication").description("Authentication API"))
+                .addTagsItem(new Tag().name("2. User Management").description("User Management API"))
+                .addTagsItem(new Tag().name("3. Order Management").description("Order Management API"))
+                .addTagsItem(new Tag().name("4. Menu Item Management").description("Menu Item Management API"));
     }
 }
