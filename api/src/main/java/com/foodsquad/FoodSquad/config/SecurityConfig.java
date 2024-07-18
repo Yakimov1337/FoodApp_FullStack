9a1e40dd82e528f732a311e7ff617ba118fb05e3
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-07-18 19:14:48 +0300

    temporary permit all routes

--- api/src/main/java/com/foodsquad/FoodSquad/config/SecurityConfig.java
+++ api/src/main/java/com/foodsquad/FoodSquad/config/SecurityConfig.java
@@ -15,32 +15,32 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable()) // Disable CSRF for simplicity
-                .authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests
-                                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll() // Allow access to Swagger UI without authentication
-                                .anyRequest().authenticated() // Secure other endpoints
-                )
-                .formLogin(formLogin -> formLogin.permitAll()) // Allow form login
-                .logout(logout -> logout.permitAll()); // Allow logout
-        return http.build();
-    }
-
 //    @Bean
 //    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 //        http
 //                .csrf(csrf -> csrf.disable()) // Disable CSRF for simplicity
 //                .authorizeHttpRequests(authorizeRequests ->
 //                        authorizeRequests
-//                                .anyRequest().permitAll() // Allow access to all routes
+//                                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll() // Allow access to Swagger UI without authentication
+//                                .anyRequest().authenticated() // Secure other endpoints
 //                )
 //                .formLogin(formLogin -> formLogin.permitAll()) // Allow form login
 //                .logout(logout -> logout.permitAll()); // Allow logout
 //        return http.build();
 //    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // Disable CSRF for simplicity
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .anyRequest().permitAll() // Allow access to all routes
+                )
+                .formLogin(formLogin -> formLogin.permitAll()) // Allow form login
+                .logout(logout -> logout.permitAll()); // Allow logout
+        return http.build();
+    }
 
 
     @Bean
