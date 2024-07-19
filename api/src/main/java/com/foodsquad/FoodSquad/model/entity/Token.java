54042d53903c3d7349dd0173436379b7cf00fc89
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-07-19 15:03:12 +0300

    Create Token.java

---
+++ api/src/main/java/com/foodsquad/FoodSquad/model/entity/Token.java
@@ -0,0 +1,64 @@
+package com.foodsquad.FoodSquad.model.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Token {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private boolean isRevoked = false;
+
+    @Column(nullable = false)
+    private LocalDateTime expiryDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public boolean isRevoked() {
+        return isRevoked;
+    }
+
+    public void setRevoked(boolean revoked) {
+        isRevoked = revoked;
+    }
+
+    public LocalDateTime getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(LocalDateTime expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+}
+
