6b56ca530fa60c0079637dc9d3bdb58e500cbd7e
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-07-21 22:00:43 +0300

    Update OpenApiConfig.java

--- api/src/main/java/com/foodsquad/FoodSquad/config/OpenApiConfig.java
+++ api/src/main/java/com/foodsquad/FoodSquad/config/OpenApiConfig.java
@@ -1,10 +1,15 @@
 package com.foodsquad.FoodSquad.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.tags.Tag;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CookieValue;
 
 @Configuration
 public class OpenApiConfig {
@@ -16,9 +21,20 @@ public class OpenApiConfig {
                         .title("Food Squad API")
                         .version("1.0")
                         .description("API documentation for Food Squad application"))
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
                 .addTagsItem(new Tag().name("1. Authentication").description("Authentication API"))
                 .addTagsItem(new Tag().name("2. User Management").description("User Management API"))
                 .addTagsItem(new Tag().name("3. Order Management").description("Order Management API"))
                 .addTagsItem(new Tag().name("4. Menu Item Management").description("Menu Item Management API"));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/api/**")
+                .build();
     }
 }
