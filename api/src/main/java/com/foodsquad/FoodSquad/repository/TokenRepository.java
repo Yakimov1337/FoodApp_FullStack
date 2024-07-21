e2b0fe895a5ddc224c29427dd25d6f72c5b92ca9
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-07-21 22:02:10 +0300

    feat: delete unused tokens

--- api/src/main/java/com/foodsquad/FoodSquad/repository/TokenRepository.java
+++ api/src/main/java/com/foodsquad/FoodSquad/repository/TokenRepository.java
@@ -1,9 +1,11 @@
 package com.foodsquad.FoodSquad.repository;
 
 import com.foodsquad.FoodSquad.model.entity.Token;
+import com.foodsquad.FoodSquad.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface TokenRepository extends JpaRepository<Token, Long> {
     Optional<Token> findByToken(String token);
+    void deleteByUser(User user);
 }
