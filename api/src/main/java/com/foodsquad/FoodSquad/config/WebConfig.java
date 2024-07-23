156819a01fca18f6a28b9cc0f842520ab3791986
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-07-23 19:35:38 +0300

    Update WebConfig.java

--- api/src/main/java/com/foodsquad/FoodSquad/config/WebConfig.java
+++ api/src/main/java/com/foodsquad/FoodSquad/config/WebConfig.java
@@ -15,9 +15,10 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
                         .allowedOrigins("http://localhost:4200")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowCredentials(true)
+                        .maxAge(3600);
             }
         };
     }
