2c07196ef280a542097c0095b78d4a58789563d4
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-07-27 18:47:49 +0300

    impl: model mapper

--- api/src/main/java/com/foodsquad/FoodSquad/service/OrderService.java
+++ api/src/main/java/com/foodsquad/FoodSquad/service/OrderService.java
@@ -3,27 +3,27 @@ package com.foodsquad.FoodSquad.service;
 import com.foodsquad.FoodSquad.model.dto.OrderDTO;
 import com.foodsquad.FoodSquad.model.entity.MenuItem;
 import com.foodsquad.FoodSquad.model.entity.Order;
+import com.foodsquad.FoodSquad.model.entity.OrderStatus;
 import com.foodsquad.FoodSquad.model.entity.User;
-import com.foodsquad.FoodSquad.model.entity.UserRole;
 import com.foodsquad.FoodSquad.repository.MenuItemRepository;
 import com.foodsquad.FoodSquad.repository.OrderRepository;
 import com.foodsquad.FoodSquad.repository.UserRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
-import org.springframework.data.domain.Pageable;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class OrderService {
+
     @Autowired
     private OrderRepository orderRepository;
 
@@ -33,32 +33,30 @@ public class OrderService {
     @Autowired
     private MenuItemRepository menuItemRepository;
 
-    private User getCurrentUser() {
-        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        return userRepository.findByEmail(userDetails.getUsername())
-                .orElseThrow(() -> new IllegalArgumentException("User not found"));
-    }
-
-    private void checkOwnership(Order order) {
-        User currentUser = getCurrentUser();
-        if (!order.getUser().equals(currentUser) && !currentUser.getRole().equals(UserRole.ADMIN) && !currentUser.getRole().equals(UserRole.MODERATOR)) {
-            throw new IllegalArgumentException("Access denied");
-        }
-    }
+    @Autowired
+    private ModelMapper modelMapper;
 
     public ResponseEntity<String> createOrder(OrderDTO orderDTO) {
         Order order = new Order();
 
-        // Set current authenticated user
-        User currentUser = getCurrentUser();
-        order.setUser(currentUser);
+        // Find user by email
+        Optional<User> userOpt = userRepository.findByEmail(orderDTO.getUserEmail());
+        if (userOpt.isPresent()) {
+            User user = userOpt.get();
+            order.setUser(user);
+        } else {
+            throw new IllegalArgumentException("User not found with email: " + orderDTO.getUserEmail());
+        }
 
         // Set menu items
         List<MenuItem> menuItems = menuItemRepository.findAllById(orderDTO.getMenuItemIds());
+        if (menuItems.isEmpty()) {
+            throw new IllegalArgumentException("Order must contain at least one MenuItem");
+        }
         order.setMenuItems(menuItems);
 
         // Set other fields
-        order.setStatus(orderDTO.getStatus());
+        order.setStatus(OrderStatus.valueOf(orderDTO.getStatus().toUpperCase()));
         order.setTotalCost(orderDTO.getTotalCost());
         order.setCreatedOn(orderDTO.getCreatedOn());
         order.setPaid(orderDTO.getPaid());
@@ -67,34 +65,51 @@ public class OrderService {
         return new ResponseEntity<>("Order successfully created.", HttpStatus.CREATED);
     }
 
-    public OrderDTO getOrderById(Long id) {
-        Optional<Order> order = orderRepository.findById(id);
-        if (order.isPresent()) {
-            checkOwnership(order.get());
-            return new OrderDTO(order.get());
-        } else {
-            throw new IllegalArgumentException("Order not found");
-        }
-    }
-
     public List<OrderDTO> getAllOrders(int page, int size) {
         Pageable pageable = PageRequest.of(page, size);
-        Page<Order> orderPage = orderRepository.findAll(pageable);
+        Page<Order> orderPage = orderRepository.findAllOrdersWithUsers(pageable);
         return orderPage.stream()
-                .map(OrderDTO::new)
+                .map(order -> modelMapper.map(order, OrderDTO.class))
                 .collect(Collectors.toList());
     }
 
-    public ResponseEntity<String> updateOrder(Long id, OrderDTO orderDTO) {
-        Optional<Order> existingOrder = orderRepository.findById(id);
-        if (existingOrder.isPresent()) {
-            Order order = existingOrder.get();
-            checkOwnership(order);
+    public List<OrderDTO> getOrdersByUserId(Long userId, int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Order> orders = orderRepository.findOrdersByUserId(userId, pageable);
+        return orders.stream()
+                .map(order -> modelMapper.map(order, OrderDTO.class))
+                .collect(Collectors.toList());
+    }
 
-            // Update fields
+    public OrderDTO getOrderById(Long id) {
+        Order order = orderRepository.findOrderWithUserById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Order not found"));
+        return modelMapper.map(order, OrderDTO.class);
+    }
+
+    public ResponseEntity<String> updateOrder(Long id, OrderDTO orderDTO) {
+        Optional<Order> existingOrderOpt = orderRepository.findById(id);
+        if (existingOrderOpt.isPresent()) {
+            Order order = existingOrderOpt.get();
+
+            // Update user
+            Optional<User> userOpt = userRepository.findByEmail(orderDTO.getUserEmail());
+            if (userOpt.isPresent()) {
+                User user = userOpt.get();
+                order.setUser(user);
+            } else {
+                throw new IllegalArgumentException("User not found with email: " + orderDTO.getUserEmail());
+            }
+
+            // Update menu items
             List<MenuItem> menuItems = menuItemRepository.findAllById(orderDTO.getMenuItemIds());
+            if (menuItems.isEmpty()) {
+                throw new IllegalArgumentException("Order must contain at least one MenuItem");
+            }
             order.setMenuItems(menuItems);
-            order.setStatus(orderDTO.getStatus());
+
+            // Update other fields
+            order.setStatus(OrderStatus.valueOf(orderDTO.getStatus().toUpperCase()));
             order.setTotalCost(orderDTO.getTotalCost());
             order.setPaid(orderDTO.getPaid());
 
@@ -106,13 +121,22 @@ public class OrderService {
     }
 
     public ResponseEntity<String> deleteOrder(Long id) {
-        Optional<Order> order = orderRepository.findById(id);
-        if (order.isPresent()) {
-            checkOwnership(order.get());
-            orderRepository.delete(order.get());
+        Optional<Order> orderOpt = orderRepository.findById(id);
+        if (orderOpt.isPresent()) {
+            orderRepository.delete(orderOpt.get());
             return new ResponseEntity<>("Order successfully deleted.", HttpStatus.NO_CONTENT);
         } else {
             return new ResponseEntity<>("Order not found", HttpStatus.NOT_FOUND);
         }
+    }
+
+
+
+    private OrderDTO convertToDTO(Order order) {
+        List<Long> menuItemIds = order.getMenuItems().stream()
+                .map(MenuItem::getId)
+                .collect(Collectors.toList());
+        return new OrderDTO(order.getId(), order.getUser().getEmail(), menuItemIds, order.getStatus(),
+                order.getTotalCost(), order.getCreatedOn(), order.getPaid());
     }
 }
