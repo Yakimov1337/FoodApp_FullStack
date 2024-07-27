00797e294990a173cfe02b58af571904452e617c
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-07-27 18:45:20 +0300

    Update UserLoginDTO.java

--- api/src/main/java/com/foodsquad/FoodSquad/model/dto/UserLoginDTO.java
+++ api/src/main/java/com/foodsquad/FoodSquad/model/dto/UserLoginDTO.java
@@ -3,14 +3,17 @@ package com.foodsquad.FoodSquad.model.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public class UserLoginDTO {
     @NotBlank(message = "Email is required")
     @Email(message = "Email should be valid")
+    @Schema(example = "admin@example.com")
     private String email;
 
     @NotBlank(message = "Password is required")
     @Size(min = 6, max = 20, message = "Size must be between 6 and 20 characters")
+    @Schema(example = "123123")
     private String password;
 
     public @Email @NotBlank String getEmail() {
