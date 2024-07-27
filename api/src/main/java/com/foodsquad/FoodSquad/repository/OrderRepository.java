46539f1918b5bdde1ab55d19b6949b0347dc0a44
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-07-27 18:46:35 +0300

    impl: custom queries

--- api/src/main/java/com/foodsquad/FoodSquad/repository/OrderRepository.java
+++ api/src/main/java/com/foodsquad/FoodSquad/repository/OrderRepository.java
@@ -1,7 +1,25 @@
 package com.foodsquad.FoodSquad.repository;
 
 import com.foodsquad.FoodSquad.model.entity.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    @Query("SELECT COUNT(o) FROM Order o JOIN o.menuItems mi WHERE mi.id = :menuItemId")
+    int countByMenuItemId(@Param("menuItemId") Long menuItemId);
+
+    @Query("SELECT o FROM Order o JOIN o.user u")
+    Page<Order> findAllOrdersWithUsers(Pageable pageable);
+
+    @Query("SELECT o FROM Order o JOIN o.user u WHERE o.id = :orderId")
+    Optional<Order> findOrderWithUserById(@Param("orderId") Long orderId);
+
+    @Query("SELECT o FROM Order o JOIN o.user u WHERE u.id = :userId")
+    Page<Order> findOrdersByUserId(@Param("userId") Long userId, Pageable pageable);
 }
