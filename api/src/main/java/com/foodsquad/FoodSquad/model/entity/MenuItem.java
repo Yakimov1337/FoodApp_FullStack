3fd70741bbb67f75d7ff29d1acf621ac5e69d7ae
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-07-30 17:38:23 +0300

    fix: length and url

--- api/src/main/java/com/foodsquad/FoodSquad/model/entity/MenuItem.java
+++ api/src/main/java/com/foodsquad/FoodSquad/model/entity/MenuItem.java
@@ -17,8 +17,8 @@ public class MenuItem {
     @Column(nullable = false)
     private String description;
 
-    @Column(nullable = false)
-    private String imageUrl = "https://www.tastingtable.com/img/gallery/what-makes-restaurant-burgers-taste-different-from-homemade-burgers-upgrade/l-intro-1662064407.jpg";
+    @Column(name = "image_url", nullable = false, length = 512)
+    private String imageUrl = "https://w0.peakpx.com/wallpaper/97/150/HD-wallpaper-mcdonalds-double-cheese-burger-double-mcdonalds-cheese-burger-thumbnail.jpg";
 
     @Column(nullable = false)
     private Boolean defaultItem = false;
