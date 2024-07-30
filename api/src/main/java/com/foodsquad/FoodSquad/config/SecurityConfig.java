8254377e79d70629df30698c5533ab317daee507
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-07-30 17:36:48 +0300

    Update SecurityConfig.java

--- api/src/main/java/com/foodsquad/FoodSquad/config/SecurityConfig.java
+++ api/src/main/java/com/foodsquad/FoodSquad/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
                                 .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                                 // User controller endpoints
                                 .requestMatchers(HttpMethod.GET, "/api/users/**").hasAnyRole("ADMIN", "MODERATOR")
-                                .requestMatchers(HttpMethod.PUT, "/api/users/**").hasAnyRole("ADMIN", "MODERATOR")
+                                .requestMatchers(HttpMethod.PUT, "/api/users/**").hasAnyRole("ADMIN", "MODERATOR", "NORMAL")
                                 .requestMatchers(HttpMethod.DELETE, "/api/users/**").hasRole("ADMIN")
                                 // Order controller endpoints
                                 .requestMatchers(HttpMethod.GET, "/api/orders/**").hasAnyRole("ADMIN", "MODERATOR", "NORMAL")
