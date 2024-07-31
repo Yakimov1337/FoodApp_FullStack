38f20e055fde47befc337cb3e5abc3cedd22e9d5
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-07-31 19:00:20 +0300

    update: find orders older than a day

--- api/src/main/java/com/foodsquad/FoodSquad/repository/OrderRepository.java
+++ api/src/main/java/com/foodsquad/FoodSquad/repository/OrderRepository.java
@@ -1,12 +1,14 @@
 package com.foodsquad.FoodSquad.repository;
 
 import com.foodsquad.FoodSquad.model.entity.Order;
+import com.foodsquad.FoodSquad.model.entity.OrderStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,4 +24,6 @@ public interface OrderRepository extends JpaRepository<Order, String> {
 
     @Query("SELECT o FROM Order o JOIN o.user u WHERE u.id = :userId")
     Page<Order> findOrdersByUserId(@Param("userId") String userId, Pageable pageable);
+
+    List<Order> findByCreatedOnBeforeAndStatusNot(LocalDateTime dateTime, OrderStatus status);
 }
