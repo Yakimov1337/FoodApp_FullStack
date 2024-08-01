c2e09dc20c4e610e36f9e8302bc877abe9640639
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-08-01 20:36:26 +0300

    impl: Review repository

---
+++ api/src/main/java/com/foodsquad/FoodSquad/repository/ReviewRepository.java
@@ -0,0 +1,15 @@
+package com.foodsquad.FoodSquad.repository;
+
+import com.foodsquad.FoodSquad.model.entity.Review;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<Review> findByMenuItemId(Long menuItemId);
+    List<Review> findByUserId(String userId);
+    List<Review> findByUserIdOrderByCreatedOnDesc(String userId, Pageable pageable);
+}
