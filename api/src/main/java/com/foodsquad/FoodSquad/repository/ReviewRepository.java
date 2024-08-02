3cf7df4ac096eb4730c6c0b34ac68de8895c1544
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-08-02 19:33:08 +0300

    impl: average menu item rating query

--- api/src/main/java/com/foodsquad/FoodSquad/repository/ReviewRepository.java
+++ api/src/main/java/com/foodsquad/FoodSquad/repository/ReviewRepository.java
@@ -3,6 +3,7 @@ package com.foodsquad.FoodSquad.repository;
 import com.foodsquad.FoodSquad.model.entity.Review;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +13,8 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findByMenuItemId(Long menuItemId);
     List<Review> findByUserId(String userId);
     List<Review> findByUserIdOrderByCreatedOnDesc(String userId, Pageable pageable);
+    long countByMenuItemId(Long menuItemId);
+
+    @Query("SELECT AVG(r.rating) FROM Review r WHERE r.menuItem.id = :menuItemId")
+    Double findAverageRatingByMenuItemId(Long menuItemId);
 }
