b797775abb2868468fc16403bbacb761c3498c60
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-08-04 21:06:10 +0300

    update: find existing access token

--- api/src/main/java/com/foodsquad/FoodSquad/repository/TokenRepository.java
+++ api/src/main/java/com/foodsquad/FoodSquad/repository/TokenRepository.java
@@ -3,11 +3,16 @@ package com.foodsquad.FoodSquad.repository;
 import com.foodsquad.FoodSquad.model.entity.Token;
 import com.foodsquad.FoodSquad.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface TokenRepository extends JpaRepository<Token, Long> {
-    Optional<Token> findByToken(String token);
-    Optional<Token> findByUserAndToken(User user, String token);
+    Optional<Token> findByUserAndAccessToken(User user, String accessToken);
+    Optional<Token> findByUserAndRefreshToken(User user, String refreshToken);
     void deleteByUser(User user);
-    void deleteByToken(String token);
+    void deleteByAccessToken(String accessToken);
+    void deleteByRefreshToken(String refreshToken);
+    boolean existsByAccessToken(String accessToken);
 }
