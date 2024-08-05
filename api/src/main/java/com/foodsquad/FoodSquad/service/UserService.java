4fe6efd47c3a9984adba03e2f62b63882892465c
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-08-05 16:04:58 +0300

    impl: user response dto

--- api/src/main/java/com/foodsquad/FoodSquad/service/UserService.java
+++ api/src/main/java/com/foodsquad/FoodSquad/service/UserService.java
@@ -2,6 +2,7 @@ package com.foodsquad.FoodSquad.service;
 
 import com.foodsquad.FoodSquad.model.dto.OrderDTO;
 import com.foodsquad.FoodSquad.model.dto.UserResponseDTO;
+import com.foodsquad.FoodSquad.model.dto.UserUpdateDTO;
 import com.foodsquad.FoodSquad.model.entity.Order;
 import com.foodsquad.FoodSquad.model.entity.User;
 import com.foodsquad.FoodSquad.model.entity.UserRole;
@@ -74,22 +75,36 @@ public class UserService {
     }
 
     @Transactional
-    public ResponseEntity<UserResponseDTO> updateUser(String id, UserResponseDTO userResponseDTO) {
+    public ResponseEntity<UserResponseDTO> updateUser(String id, UserUpdateDTO userUpdateDTO) {
+        User currentUser = getCurrentUser();
         checkOwnership(id);
+        // Check if the current user is trying to update their own role
+        if (currentUser.getId().equals(id) && !currentUser.getRole().name().equals(userUpdateDTO.getRole())) {
+            throw new IllegalArgumentException("Users cannot update their own role.");
+        }
 
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("User not found for ID: " + id));
 
+        // Check if the current user is a normal user and is trying to change the role to something other than NORMAL
+        if (currentUser.getRole().equals(UserRole.NORMAL) && !userUpdateDTO.getRole().equals(UserRole.NORMAL.name())) {
+            throw new IllegalArgumentException("Normal users cannot change roles.");
+        }
+
+        // Check if the current user is not an admin but is trying to set the role to admin
+        if (!currentUser.getRole().equals(UserRole.ADMIN) && userUpdateDTO.getRole().equals(UserRole.ADMIN.name())) {
+            throw new IllegalArgumentException("Only admin users can assign the admin role.");
+        }
+
         // Check if the user role is ADMIN and is being changed
-        if (user.getRole() == UserRole.ADMIN && !userResponseDTO.getRole().equals(UserRole.ADMIN.name())) {
+        if (user.getRole() == UserRole.ADMIN && !userUpdateDTO.getRole().equals(UserRole.ADMIN.name())) {
             throw new IllegalArgumentException("Admin user role cannot be changed");
         }
 
-        user.setName(userResponseDTO.getName());
-        user.setEmail(userResponseDTO.getEmail());
-        user.setRole(UserRole.valueOf(userResponseDTO.getRole()));
-        user.setImageUrl(userResponseDTO.getImageUrl());
-        user.setPhoneNumber(userResponseDTO.getPhoneNumber());
+        user.setName(userUpdateDTO.getName());
+        user.setRole(UserRole.valueOf(userUpdateDTO.getRole()));
+        user.setImageUrl(userUpdateDTO.getImageUrl());
+        user.setPhoneNumber(userUpdateDTO.getPhoneNumber());
 
         userRepository.save(user);
         long ordersCount = orderRepository.countByUserId(id);
@@ -112,6 +127,6 @@ public class UserService {
         });
 
         userRepository.delete(user);
-        return ResponseEntity.ok(Map.of("message","User successfully deleted."));
+        return ResponseEntity.ok(Map.of("message", "User successfully deleted."));
     }
 }
