b97efe6d7f0b3e8e18e78161a19fc46960868c56
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-08-06 15:13:31 +0300

    impl: reviews

--- client/src/app/modules/admin/dashboard/dashboard-routing.module.ts
+++ client/src/app/modules/admin/dashboard/dashboard-routing.module.ts
@@ -5,6 +5,7 @@ import { OverviewComponent } from './pages/overview/overview.component';
 import { ItemsComponent } from './pages/items/items.component';
 import { OrdersComponent } from './pages/orders/orders.component';
 import { UsersComponent } from './pages/users/users.component';
+import { ReviewsComponent } from './pages/reviews/reviews.component';
 
 const routes: Routes = [
   {
@@ -14,6 +15,7 @@ const routes: Routes = [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
       { path: 'items', component: ItemsComponent },
       { path: 'orders', component: OrdersComponent },
+      { path: 'reviews', component: ReviewsComponent },
       { path: 'users', component: UsersComponent },
       { path: 'dashboard', component: OverviewComponent },
       { path: '**', redirectTo: 'dashboard' },
--- client/src/app/modules/admin/dashboard/pages/items/items.component.ts
+++ client/src/app/modules/admin/dashboard/pages/items/items.component.ts
@@ -5,8 +5,9 @@ import { MenuItemAuctionsTableComponent } from '../../components/menuItem/menuIt
 import { MenuItemCreateModalComponent } from '../../components/menuItem/menuItem-create-modal/menuItem-create-modal.component';
 import { MenuItemDeleteModalComponent } from '../../components/menuItem/menuItem-delete-modal/menuItem-delete-modal.component';
 import { MenuItemUpdateModalComponent } from '../../components/menuItem/menuItem-update-modal/menuItem-update-modal.component';
-import { selectIsCreateMenuItemModalOpen, selectIsDeleteMenuItemModalOpen, selectIsUpdateMenuItemModalOpen } from '../../../../../core/state/modal/menuItem/modal.selectors';
-import { animate, style, transition, trigger } from '@angular/animations';
+import { UserReviewsModalComponent } from '../../components/menuItem/user-reviews-modal/user-reviews.component';
+import { selectIsCreateMenuItemModalOpen, selectIsDeleteMenuItemModalOpen, selectIsUpdateMenuItemModalOpen, selectIsUserReviewsModalOpen } from '../../../../../core/state/modal/menuItem/modal.selectors';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 @Component({
   selector: 'app-items',
   standalone: true,
@@ -16,6 +17,7 @@ import { animate, style, transition, trigger } from '@angular/animations';
     MenuItemCreateModalComponent,
     MenuItemDeleteModalComponent,
     MenuItemUpdateModalComponent,
+    UserReviewsModalComponent,
   ],
   templateUrl: './items.component.html',
   animations: [
@@ -25,12 +27,18 @@ import { animate, style, transition, trigger } from '@angular/animations';
         animate('1s ease-out', style({ transform: 'translateX(0)', opacity: 1 })),
       ]),
     ]),
+    trigger('fadeInOut', [
+      state('in', style({ opacity: 1 })),
+      transition(':enter', [style({ opacity: 0 }), animate('0.5s ease-in', style({ opacity: 1 }))]),
+      transition(':leave', [animate('0.5s ease-out', style({ opacity: 0 }))]),
+    ]),
   ],
 })
 export class ItemsComponent {
   showCreateMenuItemModal$ = this.store.select(selectIsCreateMenuItemModalOpen);
   showDeleteMenuItemModal$ = this.store.select(selectIsDeleteMenuItemModalOpen);
   showUpdateMenuItemModal$ = this.store.select(selectIsUpdateMenuItemModalOpen);
+  showUserReviewsModal$ = this.store.select(selectIsUserReviewsModalOpen);
 
   constructor(private store: Store) {}
 }
