0edfbe3512cedaa46b475cb263cc8be675380968
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-08-06 15:22:39 +0300

    Create environment.ts

---
+++ client/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  stripePublishKey: 'yourStripeKey',
+  //Credentials
+  adminCredentials: {
+    email: 'admin@admin.bg',
+    password: '123123123',
+  },
+  moderatorCredentials: {
+    email: 'moderator@moderator.bg',
+    password: '123123123',
+  },
+};
