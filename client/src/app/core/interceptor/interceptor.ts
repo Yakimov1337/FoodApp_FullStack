d184f29946864621ab4003bbcf80c593f0382d9e
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-08-07 14:57:07 +0300

    fix: requests execution order

--- client/src/app/core/interceptor/interceptor.ts
+++ client/src/app/core/interceptor/interceptor.ts
@@ -2,17 +2,17 @@ import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Observable, throwError } from 'rxjs';
-import { catchError, switchMap } from 'rxjs/operators';
+import { catchError, switchMap, finalize } from 'rxjs/operators';
 import { AuthService } from '../../services/auth.service';
 import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   private isRefreshing = false;
-  constructor(private authService: AuthService, private router: Router,private toastr: ToastrService) {}
+
+  constructor(private authService: AuthService, private router: Router, private toastr: ToastrService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    // Add the access token to the headers
     const accessToken = localStorage.getItem('accessToken');
     if (accessToken) {
       request = request.clone({
@@ -24,10 +24,13 @@ export class AuthInterceptor implements HttpInterceptor {
 
     return next.handle(request).pipe(
       catchError((error: HttpErrorResponse) => {
+        console.error('HTTP error intercepted:', error);
+
         if (error.status === 401 && !this.isRefreshing) {
           this.isRefreshing = true;
           return this.authService.refreshToken().pipe(
             switchMap((newAccessToken: string) => {
+              console.log('New access token obtained:', newAccessToken);
               localStorage.setItem('accessToken', newAccessToken);
               this.isRefreshing = false; // Reset the flag
               // Retry the failed request with the new token
@@ -39,15 +42,18 @@ export class AuthInterceptor implements HttpInterceptor {
               return next.handle(request);
             }),
             catchError((err) => {
+              console.error('Error refreshing token:', err);
               this.isRefreshing = false; // Reset the flag
-              if (err.status === 403 || err.status === 401) { // Handle the custom status code
+              if (err.status === 403 || err.status === 401) {
                 this.toastr.info("Session expired. Please log in again.");
               }
               localStorage.removeItem('accessToken');
               sessionStorage.clear();
-              // If refresh token fails, redirect to sign-in page
               this.router.navigate(['/auth/sign-in']);
               return throwError(() => new Error('Session expired. Please log in again.'));
+            }),
+            finalize(() => {
+              this.isRefreshing = false; // Reset the flag in case of any error
             }),
           );
         } else {
