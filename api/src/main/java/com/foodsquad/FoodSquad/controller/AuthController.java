5daa5405b55e81e65a583226e9fca8e0d7612590
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-08-31 11:12:47 +0300

    fix: type response cookie added

--- api/src/main/java/com/foodsquad/FoodSquad/controller/AuthController.java
+++ api/src/main/java/com/foodsquad/FoodSquad/controller/AuthController.java
@@ -19,7 +19,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.validation.annotation.Validated;
@@ -84,12 +86,15 @@ public class AuthController {
 
         tokenService.saveTokens(user.getEmail(), accessToken, refreshToken);
 
-        Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
-        refreshTokenCookie.setHttpOnly(true);
-        refreshTokenCookie.setSecure(true);
-        refreshTokenCookie.setPath("/");
-        refreshTokenCookie.setMaxAge((int) refreshTokenExpiration / 1000);
-        response.addCookie(refreshTokenCookie);
+        ResponseCookie refreshTokenCookie = ResponseCookie.from("refreshToken", refreshToken)
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(refreshTokenExpiration / 1000)
+                .sameSite("None")
+                .build();
+
+        response.addHeader(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString());
 
         Map<String, String> tokens = new HashMap<>();
         tokens.put("accessToken", accessToken);
